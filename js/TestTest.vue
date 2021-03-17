f72af7d65b7b04ed637599027b06a956d3b1a271
Luke <luke@nearbeach.org>
2021-03-17 21:01:24 +1100

    First commit

---
+++ js/TestTest.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<script>
+// Import component
+import IconifyIcon from '@iconify/vue';
+import homeIcon from '@iconify-icons/mdi-light/home';
+
+export default {
+    name: "TestTest",
+    props: {},
+    components: {
+        IconifyIcon,
+    },
+    data() {
+       return {
+           // Returns icons data that can be used in template
+           icons: {
+               // Assign homeIcon to icons.home
+               //home: homeIcon,
+           },
+       };
+   },
+}
+</script>
+
---
+++ test/unit/app.spec.js
@@ -0,0 +1,7 @@
+import { mount } from '@vue/test-utils';
+import TestTest from '../../js/TestTest';
+
+test('Simple True be True', () => {
+    expect(true).toBe(true);
+});
+
